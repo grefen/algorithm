a1088d882df9192080605581dfc5a3418ec69146
grefen <ccbp2003@163.com>
2016-11-09 23:59:18 +0800

    min stack

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <cstdio>
-
+#include<algorithm>
+#include<exception>
+#include<iostream>
+#include"GetMinStack.h"
 int main()
 {
     printf("hello from algorithm!\n");
+
+	GetMinStack<int, std::less<int>> minStack;
+
+	minStack.push(3);
+	minStack.push(4);
+	minStack.pop();
+	std::cout << minStack.getMin() << std::endl;
+
+
     return 0;
 }
