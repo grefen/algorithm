fa958cecdc1321a32c037415d6fef65fff01fb26
grefen <ccbp2003@163.com>
2016-11-10 21:40:31 +0800

    third

--- main.cpp
+++ main.cpp
@@ -3,16 +3,25 @@
 #include<exception>
 #include<iostream>
 #include"GetMinStack.h"
+#include"TwoStacksQueue.h"
 int main()
 {
     printf("hello from algorithm!\n");
 
-	GetMinStack<int, std::less<int>> minStack;
 
-	minStack.push(3);
-	minStack.push(4);
-	minStack.pop();
-	std::cout << minStack.getMin() << std::endl;
+	//GetMinStack<int, std::less<int>> minStack;
+	//minStack.push(3);
+	//minStack.push(4);
+	//minStack.pop();
+	//std::cout << minStack.getMin() << std::endl;
+
+	TwoStacksQueue<int> twoStack;
+	twoStack.push(1);
+	twoStack.push(2);
+	twoStack.push(3);
+	
+	twoStack.pop();
+	twoStack.top();
 
 
     return 0;
